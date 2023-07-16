b3f9b1755a22320cb2fe7f2c4307576171442947
xukunliu2000 <57486241+liuxukun2000@users.noreply.github.com>
2023-07-16 15:04:42 +0800

    log mode (#39)

--- gentopia/output/__init__.py
+++ gentopia/output/__init__.py
@@ -2,7 +2,7 @@ import logging
 import os
 
 
-def enable_log(path: str = "./agent.log", log_level: str= "info" ):
+def enable_log(path: str = "./agent.log", log_level: str= "info", mode: str = "w" ):
     """
         Enables logging for the application.
 
@@ -15,7 +15,9 @@ def enable_log(path: str = "./agent.log", log_level: str= "info" ):
         path = "./agent.log"
     os.environ["LOG_LEVEL"] = log_level
     os.environ["LOG_PATH"] = path
-    logging.basicConfig(level=log_level.upper(), filename=path)
+    assert log_level.upper() in ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"], "Invalid log level."
+    assert mode in ["w", "a"], "Invalid mode."
+    logging.basicConfig(level=log_level.upper(), filename=path, filemode=mode)
 
 
 
