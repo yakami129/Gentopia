480677f90ec3e0b975ad97a73631c60373929c52
billxbf <billxbf@gmail.com>
2023-07-26 23:46:20 -0400

    clean up for docs

--- examples/agent_as_plugin/agent_as_plugin.py
+++
@@ -1,9 +0,0 @@
-from gentopia.assembler.agent_assembler import AgentAssembler
-from gentopia.output import enable_log
-from gentopia import chat
-
-if __name__ == '__main__':
-    enable_log()
-    assembler = AgentAssembler(file='configs/openai_template.yaml')
-    agent = assembler.get_agent()
-    chat(agent, verbose=True)
