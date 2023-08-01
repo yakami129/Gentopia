4d63d05bd4bc5ad444209cc045475c74d4550245
billxbf <billxbf@gmail.com>
2023-08-01 19:57:25 -0400

    update package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gentopia',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(exclude=['llm', 'llm.*']),
     url='https://github.com/Gentopia-AI/Gentopia',
     license='MIT license',
